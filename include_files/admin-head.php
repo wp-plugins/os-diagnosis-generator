b6f0e31b61109be8fdc6f41c4f39d2511daccebe
olivesystem <olivesystem@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-11 05:50:03 +0000

    1.1.0+1
    git-svn-id: https://plugins.svn.wordpress.org/os-diagnosis-generator/trunk@1086936 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include_files/admin-head.php
@@ -0,0 +1,12 @@
+<?php
+if(class_exists('DiagnosisAdmin')){
+?>
+
+		<div id="diagnosis-header" class="clearfix">
+			<div class="plugin-name">診断ジェネレータ作成プラグイン 1.1.0</div>
+			<div class="header-link"><a href="http://olivesystem.jp/lp/wordpress" target="_blank">サイト制作</a> | <a href="http://olivesystem.jp/lp/wordpress%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E9%96%8B%E7%99%BA" target="_blank">プラグイン開発</a> | <a href="http://olivesystem.jp/lp/license#diagnosis" target="_blank">ライセンス取得</a></div>
+		</div>
+
+<?php
+}
+?>
